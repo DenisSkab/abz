dc7b47262cae510952ceb9e416654fadd0c1bea5
SkabDenis <deniswork1990@gmail.com>
2024-03-05 19:48:49 +0200

    fix ceo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ import style from './header.module.scss';
 const Header = () => {
   return (
     <header className={style.header}>
-      <img src={logo} alt='logo' />
+      <img src={logo} alt='logo' width={104} height={26} />
       <div className={style.buttons}>
         <Button text='Users' link='users' />
         <Button text='Sing up' link='sing_up' />
